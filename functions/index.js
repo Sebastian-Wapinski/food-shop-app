30c8d85958b76b7e0dc03ca2008f178c11e71852
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-11-29 19:48:27 +0100

    basic deploy

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+
+const functions = require("firebase-functions")
+const express = require("express")
+const cors = require("cors")
+const app = express()
+app.use(cors({
+  origin: true,
+}))
+app.use(
+    express.json({
+      verify: (req, res, buffer) => {
+        req.rawBody = buffer
+      },
+    }),
+)
+
+const createCheckoutSession = require("./modules/createCheckoutSession")
+const webhook = require("./modules/webhook")
+
+app.post("/create-checkout-session", createCheckoutSession)
+
+app.post("/webhook", webhook)
+
+exports.stripeConnection = functions.https.onRequest(app)
