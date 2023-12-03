6c36482f0e222c387ed424e6bb21fcdc1a2a550e
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-12-03 09:24:04 +0100

    CartProduct responsiveness added

--- src/components/CartProduct/CartProduct.styled.js
+++ src/components/CartProduct/CartProduct.styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import Typography from '../Typography/Typography'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { responsiveSizes } from '../style/responsiveSizes'
 
 const DefaultStyledCartProduct = styled.div`
 display: flex;
@@ -8,6 +9,13 @@ justify-content: space-between;
 padding: 0.5rem 0.5rem;
 margin: 0.5rem 0.5rem;
 width: 80%;
+
+@media (max-width: ${responsiveSizes.mediumSizeCart}) {
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid ${props => props.theme.mainColor};
+}
 `
 
 const StyledCartProduct = styled(DefaultStyledCartProduct)(
@@ -25,6 +33,11 @@ justify-content: center;
 align-items: flex-start;
 flex-direction: column;
 width: 15rem;
+
+@media (max-width: ${responsiveSizes.mediumSizeCart}) {
+  padding: 0.5rem 0;
+  align-items: center;
+}
 `
 
 const StyledName = styled(Typography)`
@@ -47,6 +60,10 @@ display: flex;
 justify-content: space-around;
 align-items: center;
 flex-direction: column;
+
+@media (max-width: ${responsiveSizes.mediumSizeCart}) {
+  padding: 0.5rem 0;
+}
 `
 
 const StyledPrice = styled(Typography)`
@@ -61,11 +78,19 @@ const StyledAccessibility = styled(Typography)`
 display: block;
 text-align: center;
 font-size: 0.7rem;
+
+@media (max-width: ${responsiveSizes.mediumSizeCart}) {
+  padding: 0.5rem 0;
+}
 `
 
 const StyledButtonContainer = styled.div`
 display: flex;
 align-items: center;
+
+@media (max-width: ${responsiveSizes.mediumSizeCart}) {
+  padding: 1rem 0;
+}
 `
 
 const StyledErrorMessage = styled(Typography)`
--- src/components/CartProductDeliveryPayment/CartProductDeliveryPayment.styled.js
+++ src/components/CartProductDeliveryPayment/CartProductDeliveryPayment.styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import Typography from '../Typography/Typography'
+import { responsiveSizes } from '../style/responsiveSizes'
 
 const DefaultStyledCartProductDeliveryPayment = styled.div`
 display: flex;
@@ -7,6 +8,13 @@ justify-content: space-between;
 padding: 0.5rem 0.5rem;
 margin: 0.5rem 0.5rem;
 width: 80%;
+
+@media (max-width: ${responsiveSizes.mediumSizeCart}) {
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid ${props => props.theme.mainColor};
+}
 `
 
 const StyledCartProductDeliveryPayment = styled(DefaultStyledCartProductDeliveryPayment)(
@@ -27,6 +35,11 @@ align-items: center;
 font-size: 2rem;
 width: 7rem;
 margin-right: 5rem;
+
+@media (max-width: ${responsiveSizes.mediumSizeCart}) {
+  margin-right: 0;
+  padding-bottom: 2rem;
+}
 `
 
 export {
