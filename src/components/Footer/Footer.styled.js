53ee973662a86f0891ab68e85758a5c18f0b6cd2
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-12-03 10:03:42 +0100

    Footer responsiveness added

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components'
 import Typography from '../Typography/Typography'
+import { responsiveSizes } from '../style/responsiveSizes'
 
 const DefaultStyledFooter = styled.div`
 width: 100%;
 background-color: ${props => props.theme.mainColor};
 margin-top: 2rem;
+
+@media (max-width: ${responsiveSizes.mediumSize}) {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  height: 9.5rem;
+}
 `
 
 const StyledFooter = styled(DefaultStyledFooter)(
@@ -16,6 +24,10 @@ display: flex;
 justify-content: center;
 align-items: center;
 padding: 1rem 0;
+
+@media (max-width: ${responsiveSizes.mediumSize}) {
+  width: ${responsiveSizes.mobileMenu};
+}
 `
 
 export { StyledFooter, TextContainer }
