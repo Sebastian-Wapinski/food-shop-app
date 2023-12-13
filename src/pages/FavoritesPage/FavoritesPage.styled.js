ba35b8594e346cf819f0139fb9aa8878ae0d7597
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-12-13 15:41:37 +0100

    Favorites and PaymentStatus Pages responsive width added

--- src/pages/FavoritesPage/FavoritesPage.styled.js
+++ src/pages/FavoritesPage/FavoritesPage.styled.js
@@ -5,6 +5,18 @@ import Typography from '../../components/Typography/Typography'
 const DefaultStyledFavoritesPage = styled.div`
 min-height: calc(100vh - 234.59px);
 width: ${responsiveSizes.pageWidth};
+
+@media (max-width: ${responsiveSizes.pageWidth}) {
+  width: ${responsiveSizes.mediumSize};
+}
+
+@media (max-width: ${responsiveSizes.mediumSize}) {
+  width: ${responsiveSizes.smallSize};
+}
+
+@media (max-width: ${responsiveSizes.smallSize}) {
+  width: ${responsiveSizes.mobileMenu};
+}
 `
 
 const StyledFavoritesPage = styled(DefaultStyledFavoritesPage)(
